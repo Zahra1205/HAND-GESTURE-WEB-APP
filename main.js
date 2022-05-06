73d332c0acca77193a339c1e1cae32efdf270f5c
Zahra1205 <78679374+Zahra1205@users.noreply.github.com>
2022-05-06 15:46:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,3 +25,41 @@ function speak(){
     var utter_this=new SpeechSynthesisUtterance(speak_data1+speak_data2);
     synth.speak(utter_this);
 }
+function check(){
+    var img=document.getElementById("snapshot");
+    classifier.classify(img ,gotResults);
+}
+function gotResults(){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+        document.getElementById("result_emotionName1").innerHTML=result[0].label;
+        document.getElementById("result_emotionName2").innerHTML=result[1].label;
+        prediction_1=result[0].label;
+        prediction_2=result[1].label;
+        speak();
+        if(prediction_1=="victory"){
+            document.getElementById("update_emoji1").innerHTML="&#9996;";
+        }
+        if(prediction_1=="best"){
+            document.getElementById("update_emoji1").innerHTML="&#128076;";
+        }
+        if(prediction_1=="amazing"){
+            document.getElementById("update_emoji1").innerHTML="&#128077;";
+        }
+       
+        if(prediction_2=="victory"){
+            document.getElementById("update_emoji1").innerHTML="&#9996;";
+        }
+        if(prediction_2=="best"){
+            document.getElementById("update_emoji1").innerHTML="&#128076;";
+        }
+        if(prediction_2=="amazing"){
+            document.getElementById("update_emoji1").innerHTML="&#128077;";
+        }
+       
+    }
+    }
+    
